153d3c12260aa0f04acd0459afca9d182188accd
amangupta20044 <a.mann20044@gmail.com>
2026-02-26 17:08:36 +0530

    google_media_pipeline

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+import FaceExpression from './feature/expression/components/FaceExpression'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <FaceExpression />
+  )
+}
+
+export default App
